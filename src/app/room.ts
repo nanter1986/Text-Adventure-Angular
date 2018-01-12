54a0f44c26159db15e6b91a6b15a2152e4004af5
nanter1986 <nanter1986@gmail.com>
2018-01-12 09:50:20 +0200

    successful input

--- src/app/room.ts
+++ src/app/room.ts
@@ -1,4 +1,14 @@
-export class Room{
+export abstract class Room{
   imagePath: string;
   title: string;
+
+  constructor(public path:string,public titleForRoom: string){
+    this.imagePath=path;
+    this.title=titleForRoom;
+  }
+
+  abstract button1():void;
+  abstract button2():void;
+  abstract button3():void;
+  abstract button4():void;
 }
